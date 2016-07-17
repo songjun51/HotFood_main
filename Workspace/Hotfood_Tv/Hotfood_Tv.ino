70f45fa8d355b26b85f0d2986c38e6764ada8eff
songjun51 <swj8530@naver.com>
2016-07-18 04:02:49 +0900

    done

--- Workspace/Hotfood_Tv/Hotfood_Tv.ino
+++ Workspace/Hotfood_Tv/Hotfood_Tv.ino
@@ -1,33 +1,25 @@
- #include <SoftwareSerial.h>
+#include <SoftwareSerial.h>
 SoftwareSerial bluetooth(2, 3);
 int relay=10;
 boolean toggle=0;
 void setup() 
 {
-  Serial.begin(9600);
   bluetooth.begin(9600);
+  Serial.begin(9600);
+  pinMode(relay,OUTPUT);
 }
 void loop()
 {
-  if (bluetooth.available()) {  
-    Serial.write(bluetooth.read());
-  }
-  if (Serial.available()) {
-    bluetooth.write(Serial.read());
-  }
-
   if(bluetooth.read()=='1'){
-  toggle+=1;
-  if(toggle==1){
+
+  Serial.println("1");
   digitalWrite(relay,LOW);
-  delay(1000);
-  digitalWrite(relay,HIGH);
-  }
+  
   }
   else if(bluetooth.read()=='0'){
+    Serial.println("0");
   digitalWrite(relay,HIGH);
-  toggle=0;
+
   }
-  
-  delay(1000);
+ 
 }
